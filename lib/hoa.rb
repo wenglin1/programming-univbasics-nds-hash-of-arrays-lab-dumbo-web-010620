0c43105669a9a6630e295b38908ef0a9ff7aefff
weng lin <wenglin@buffalo.edu>
2019-12-20 05:51:28 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,6 +6,6 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  add_character[show][name]# Write your implementation here
+  show[name]# Write your implementation here
   # Should return the array of the 'show' argument
 end
